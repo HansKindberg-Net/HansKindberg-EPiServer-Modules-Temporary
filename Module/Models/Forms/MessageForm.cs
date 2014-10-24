0a9a873197b91bc8a38052b428fe1378651da011
Hans Kindberg <hans.kindberg@gmail.com>
2014-10-24 16:00:49 +0200

    Code cleanup

--- Module/Models/Forms/MessageForm.cs
+++ Module/Models/Forms/MessageForm.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace HansKindberg.EPiServer.ModuleTemplate.Models.Forms
+﻿namespace HansKindberg.EPiServer.ModuleTemplate.Models.Forms
 {
-	public class MessageForm
-	{
-	}
+	public class MessageForm {}
 }
